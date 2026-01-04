2900657fdbe59336b11b175ff1d3d10b95e5a9c0
easzkamaai-hub <easzkamaai@gmail.com>
2026-01-04 14:37:21 +0530

    Refactor environment variable assignments in config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 # config.py
 import os
 
-API_ID = int(os.environ.get("26576868", 0))
-API_HASH = os.environ.get("7c091067324e2f3c6c7cdf8e575af4c8", "")
-BOT_TOKEN = os.environ.get("7754868963:AAFikhhRpVsDA--LYVOVpONiglPC9Ai7Lek", "")
-TMDB_API_KEY = os.environ.get("03985d11f17343d76561cebc240f5a32", "")  # TMDB API key
+API_ID = int(os.environ.get("26576868")
+API_HASH = os.environ.get("7c091067324e2f3c6c7cdf8e575af4c8")
+BOT_TOKEN = os.environ.get("7754868963:AAFikhhRpVsDA--LYVOVpONiglPC9Ai7Lek")
+TMDB_API_KEY = os.environ.get("03985d11f17343d76561cebc240f5a32")  # TMDB API key
 WEBSITE = os.environ.get("https://www.filmyfiy.mov/site-1.html?to-search=", "https://example.com")
-ADMIN = os.environ.get("6328021097", "")        # admin id(s) comma separated or username
-CHANNELS = os.environ.get("-1003618747014", "")  # other channels (comma separated)
-DATABASE_URL = os.environ.get("mongodb+srv://<db_username>:pusparaj1@pusparaj.klkgcoz.mongodb.net/?appName=Pusparaj", "")
-LOG_CHANNEL = os.environ.get("-1003692212713", "")
+ADMIN = os.environ.get("6328021097")        # admin id(s) comma separated or username
+CHANNELS = os.environ.get("-1003618747014")  # other channels (comma separated)
+DATABASE_URL = os.environ.get("mongodb+srv://<db_username>:pusparaj1@pusparaj.klkgcoz.mongodb.net/?appName=Pusparaj")
+LOG_CHANNEL = os.environ.get("-1003692212713")
 # Private channels where you forward movies — comma separated chat ids or @usernames
-PRIVATE_CHANNELS = os.environ.get("https://t.me/+D16GXeCa2fpkODQ1", "")
+PRIVATE_CHANNELS = os.environ.get("-1003550652957")
 # Backup channel where bot may copy messages
-BACKUP_CHANNEL = os.environ.get("https://t.me/+1a3O_uyhtYQ4MjM9", "")
+BACKUP_CHANNEL = os.environ.get("-1003300889716")
